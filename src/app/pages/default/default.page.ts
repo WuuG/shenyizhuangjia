8f6c4099edc9a6fd23bd4d7723bdd13049782b2a
Wuu-Server <1249732457@qq,com>
2020-10-24 21:48:34 +0800

    text

--- src/app/pages/default/default.page.ts
+++ src/app/pages/default/default.page.ts
@@ -22,7 +22,7 @@ export class DefaultPage implements OnInit {
     const lastyearweek = Math.random() * 10 * 7;
     const month = Math.random() * 10 * 30;
     const lastyearmonth = Math.random() * 10 * 30;
-
+    // 这里是测试啦
     return [
       { title: '今日', content: '比昨日', previous: yesterday, current: today },
       { title: '本周', content: '比同期', previous: lastyearweek, current: week },
